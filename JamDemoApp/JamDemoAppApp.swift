2f238fc632bb348cd8813f98430dea8e5a837af1
Gokul <gokul.g@external.nexigroup.com>
2024-02-01 13:26:47 +0530

    Initial Commit

---
+++ JamDemoApp/JamDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamDemoAppApp.swift
+//  JamDemoApp
+//
+//  Created by Gokul on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JamDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
